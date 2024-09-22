4c56ba6484fea9d29c13f6a9eac76b5ff6ab0999
Jorge Bernal <jorgebernal@usf.edu>
2024-09-22 16:57:35 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,36 @@
+// Task 1: Create an Employees Array of Employee Objects
+
+const employees = [
+    {name: 'Hazel',
+        shifts: [
+            {day: "Monday", hours: 8},
+            {day: "Tuesday", hours: 6},
+            {day: "Wednesday", hours: 4},
+            {day: "Thursday", hours: 8},
+            {day: "Friday", hours: 4},
+        ]
+    },
+    {name: 'Steven',
+        shifts: [
+            {day: "Tuesday", hours: 6},
+            {day:"Thursday", hours: 8},
+            {day: "Friday", hours: 10},
+        ]
+    },
+    {name: 'Ricky',
+        shifts: [
+            {day: "Monday", hours: 9},
+            {day:"Tuesday", hours: 10},
+            {day: "Wednesday", hours: 6},
+            {day: "Thursday", hours: 8},
+        ] 
+    },
+    {name: 'Alan',
+        shifts: [
+            {day:"Thursday", hours: 8},
+            {day:"Friday", hours: 6},
+            {day:"Saturday", hours: 8},
+            {day:"Sunday", hours: 9},
+        ]
+    },
+];
